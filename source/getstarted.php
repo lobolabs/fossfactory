ab3d752777f42aebc4e5cd05fc6ae24c9e9359cb
FOSS Factory <fossfactory@fossfactory.org>
2008-12-29 17:18:30 -0500

    Added references to the code repository.

--- source/getstarted.php
+++ source/getstarted.php
@@ -20,50 +20,25 @@ if( file_exists("/home/git/$repoproject.git")) {
 ?>
 <div style='float:right;white-space:nowrap;border:1px solid black;background-color:#ffffd0;padding:1em'>
 Project ID: <b><tt><?=$id?></tt></b><br><br>
-Git repository:<br><b><tt><?=htmlentities($repo)?></tt></b>
+Git repository:<br><b><tt><?=htmlentities($repo)?></tt></b><br><br>
+<? if( file_exists("/home/git/$repoproject.git")) { ?>
+<a href="/gitweb?p=<?=$repoproject?>.git">Browse Git Repository</a>
+<? } ?>
 </div>
-<h2>1. Install the ff Script</h2>
-
-<p>
-To work on FOSS Factory projects, start by installing the
-<b><tt>ff</tt></b> script on your local system.  This script provides
-a command-line interface for several FOSS Factory features.  Feel
-free to <a href="ff">view the script</a> before installing it.
-</p>
-<p>
-To install it, just place it anywhere in your path and make sure it's
-executable.  For example, you could type this as root:
-</p>
-<p style='margin-left:3em'><b><tt>
-curl http://www.fossfactory.org/ff -o /usr/local/bin/ff<br>
-chmod a+x /usr/local/bin/ff
-</tt></b></p>
-<p>
-To use the script, start by typing <b><tt>ff help</tt></b>.  You can get
-more detailed help on any of the commands by typing
-<b><tt>ff help <i>&lt;command&gt;</i></tt></b>.
-</p>
-
-<h2>2. Set up Git access</h2>
-<p>
-To gain access to FOSS Factory's Git repositories, type:
-</p>
-<p style='margin-left:3em'><b><tt>ff setup</tt></b></p>
-<p>
-This will transfer your SSH public key to the FOSS Factory server.
-</p>
-
-<h2>3. Download the Code</h2>
+<h2>1. Download the code</h2>
 <?
 if( file_exists("/home/git/$repoproject.git")) {
     if( $repoproject == $id) {
 ?>
 <p>
 To work on this project, you will probably want to start by downloading
-its Git repository as follows:
+its Git repository as follows: (or just <a href="/gitweb?p=<?=$repoproject?>.git">browse it online</a>.)
 </p>
 <p style='margin-left:3em'>
 <b><tt>git clone git@<?=$hostname?>:<?=$repoproject?></tt></b>
+</p>
+<p>
+If it asks you for a password, enter "guest".
 </p>
 <?
     } else {
@@ -95,7 +70,7 @@ create one from a source directory on your local filesystem.
 }
 ?>
 
-<h2>4. Make your changes</h2>
+<h2>2. Make your changes</h2>
 <p>
 If you need an introduction to Git, <a href="http://git.or.cz/">the Git homepage</a> is a good place to start.
 </p>
@@ -103,6 +78,39 @@ If you need an introduction to Git, <a href="http://git.or.cz/">the Git homepage
 In short, just edit the code and commit your changes (to your local
 repository) using <b><tt>git commit</tt></b>.  Note that when it's time to
 submit your work, only committed changes will be part of the submission.
+</p>
+
+<h2>3. Install the ff Script</h2>
+
+<p>
+To submit your changes, you'll need to install the
+<b><tt>ff</tt></b> script on your local system.  This script provides
+a command-line interface for several FOSS Factory features.  Feel
+free to <a href="ff">view the script</a> before installing it.
+</p>
+<p>
+To install it, just place it anywhere in your path and make sure it's
+executable.  For example, if you are running Ubuntu, you could type this:
+</p>
+<p style='margin-left:3em'><b><tt>
+sudo curl http://www.fossfactory.org/ff -o /usr/local/bin/ff<br>
+sudo chmod a+x /usr/local/bin/ff
+</tt></b></p>
+<p>
+To use the script, start by typing <b><tt>ff help</tt></b>.  You can get
+more detailed help on any of the commands by typing
+<b><tt>ff help <i>&lt;command&gt;</i></tt></b>.
+</p>
+
+<h2>4. Set up Git write-access</h2>
+<p>
+To gain special access to FOSS Factory's Git repositories, type:
+</p>
+<p style='margin-left:3em'><b><tt>ff setup</tt></b></p>
+<p>
+This will transfer your SSH public key to the FOSS Factory server.
+Note that you will only have push access to repositories for which you
+are the project lead, and to your own code submission repositories.
 </p>
 
 <h2>5. Submit your work</h2>
